112fa86175856a059de77af2f888fd4104cdfa2e
geek-yang <y.liu@esciencecenter.nl>
2018-07-31 18:21:51 +0200

    update tools for statistics

---
+++ meta/statistics.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright Netherlands eScience Center
+Function        : Statistical Operator for Climate Data
+Author          : Yang Liu (y.liu@esciencecenter.nl)
+First Built     : 2018.07.26
+Last Update     : 2018.07.30
+Contributor     :
+Description     : This module provides several methods to perform statistical
+                  analysis on MET and all kinds of fields.
+Return Values   : pngs
+Caveat!         :
+"""
+
+import numpy as np
+from scipy import stats
+import cartopy
+import os
+import iris
+import matplotlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+#import statistics
+
+class operator:
+    def __init__(self, var):
+        """
+        Statistical operations on climate data.
+        param var: imput time series
+        param outpath: the path for the output files
+        """
+        self.var = var
+
+    def anomaly(self, Dim_month=True):
+        """
+        Remove seasonal cycling for monthly data.
+        param Dim_month: there are two modes for removing the seasonal cycling
+        -True (default) input time series have month dimension [year,month,...]
+        -False input time series have only 1 dimension for time
+        param white_var: time series without seasonal cycling
+        """
+        # white refers to the time series without seasonal cycling
+        white_var = np.zeros(self.var.shape, dtype=float)
+        #switch mode
+        if Dim_month == True:
+            print ('The input data has the dimension of month.')
+            # check the dimension of input
+            seansonal_cycle_var = np.mean(self.var, axis=0)
+            if self.var.ndim == 2:
+                t, m = white_var.shape
+                for i in np.arange(t):
+                    for j in np.arange(m):
+                        white_var[i,j] = self.var[i,j] - seansonal_cycle_var[j]
+                # re-arrange into single time series - without month dimension
+                white_var = white_var.reshape(t*m)
+            elif self.var.ndim == 3:
+                t, m, y = white_var.shape
+                for i in np.arange(t):
+                    for j in np.arange(m):
+                        white_var[i,j,:] = self.var[i,j,:] - seansonal_cycle_var[j,:]
+                # re-arrange into single time series - without month dimension
+                white_var = white_var.reshape(t*m,y)
+            elif self.var.ndim == 4:
+                t, m, y, x = white_var.shape
+                for i in np.arange(t):
+                    for j in np.arange(m):
+                        white_var[i,j,:,:] = self.var[i,j,:,:] - seansonal_cycle_var[j,:,:]
+                # re-arrange into single time series - without month dimension
+                white_var = white_var.reshape(t*m,y,x)
+            else:
+                raise IOError("This module can not work with any array with a \
+                              dimension higher than 4!")
+        else:
+            print ('The input data does not have the dimension of month.')
+            if self.var.ndim == 1:
+                for i in np.arange(12):
+                    seansonal_cycle_var = np.mean(self.var[i::12],axis=0)
+                    white_var[i::12] = self.var[i::12] - seansonal_cycle_var
+            elif self.var.ndim == 2:
+                for i in np.arange(12):
+                    seansonal_cycle_var = np.mean(self.var[i::12,:],axis=0)
+                    white_var[i::12,:] = self.var[i::12,:] - seansonal_cycle_var
+            elif self.var.ndim == 3:
+                for i in np.arange(12):
+                    seansonal_cycle_var = np.mean(self.var[i::12,:,:],axis=0)
+                    white_var[i::12,:,:] = self.var[i::12,:,:] - seansonal_cycle_var
+            else:
+                raise IOError("This module can not work with any array with a \
+                              dimension higher than 3!")
+        self._anomaly = white_var
+        
+        print ("The output anomaly time series only contains one dimension for time!")
+        
+        return self._anomaly
+
+    def detrend(self, order=2, obj='anomaly'):
+        """
+        Detrend time series through polynomial fit.
+        param series: input time series, either 1D or 2/3D
+        param order: order of polynomial for fitting
+        param obj: objects for detrending, two options available
+        -'anomaly' (default) the time series of anomaly will be detrended
+        -'original' the original input time series will be detrended
+        """
+        if obj == 'anomaly':
+            series = self._anomaly
+        elif obj == 'original':
+            print ("Make sure that the input time series has only 1 dimension for time!")
+            series = self.var
+        else:
+            raise IOError("Please choose the right input mode for detrending!")
+        # check the dimension of input
+        if series.ndim == 1:
+            polynomial = np.polyfit(np.arange(len(series)), series, order)
+            poly_fit = np.poly1d(polynomial)
+            poly_fit_var = poly_fit(np.arange(len(series)))
+        elif series.ndim == 2:
+            poly_fit_var = np.zeros(series.shape, dtype=float)
+            t, y = poly_fit_var.shape
+            for i in np.arange(y):
+                polynomial = np.polyfit(np.arange(t), series[:,i], order)
+                poly_fit = np.poly1d(polynomial)
+                poly_fit_var[:,i] = poly_fit(np.arange(t))
+        elif series.ndim == 3:
+            poly_fit_var = np.zeros(series.shape, dtype=float)
+            t, y, x = poly_fit_var.shape
+            for i in np.arange(y):
+                for j in np.arange(x):
+                    polynomial = np.polyfit(np.arange(t), series[:,i,j], order)
+                    poly_fit = np.poly1d(polynomial)
+                    poly_fit_var[:,i,j] = poly_fit(np.arange(t))
+        else:
+            raise IOError("This module can not work with any array with a \
+                            dimension higher than 3!")
+        self._polyfit = poly_fit_var
+        self._detrend = series - self._polyfit
+
+        return self._detrend
+    
+    def lowpass(self, window=60, obj='anomaly'):
+        """
+        Apply low pass filter to the time series. The function gives running mean
+        for the point AT The End Of The Window!!
+        param series: input time series, either 1D or 2/3D
+        param window: time span for the running mean
+        param obj: object for detrending, two options available
+        -'anomaly' (default) apply low pass filter to the time series of anomaly
+        -'original' apply lowpass filter to the original input time series
+        -'detrend' apply lowpass filter to the detrended time series
+        """
+        if obj == 'anomaly':
+            series = self._anomaly
+        elif obj == 'original':
+            series = self.var
+        elif obj == 'detrend'
+            series = self._detrend        
+        # check the dimension of input
+        if series.ndim == 1:
+            t = series.shape
+            running_mean = np.zeros(t-window+1, dtype=float)
+            for i in np.arange(t-window+1):
+                running_mean[i] = np.mean(series[i:i+window])
+        elif series.ndim == 2:
+            t, y  = series.shape
+            running_mean = np.zeros((t-window+1, y), dtype=float)
+            for i in np.arange(t-window+1):
+                running_mean[i,:,:] = np.mean(series[i:i+window,:,:],0)
+        elif series.ndim == 3:
+            t, y, x = series.shape
+            running_mean = np.zeros((t-window+1, y, x), dtype=float)
+            for i in np.arange(t-window+1):
+                running_mean[i,:,:] = np.mean(series[i:i+window,:,:],0)
+        else:
+            raise IOError("This module can not work with any array with a \
+                            dimension higher than 3!")
+        self._lowpass = running_mean
+        
+        return self._lowpass
+    
+    @staticmethod
+    def linearRegress(var_x, var_y, lag=0):
+        """
+        Linear regression of input time series. Lead/lag regression can also be performed.
+        param series: input time series, either 1D or 2/3D
+        
+        """
+        # check the dimensions of input time series
+        if var_x.shape == var_y.shape:
+            pass
+        else:
+            IOError("The dimensions of input arrays do not match!")
+        # check the dimension of input
+        # do the regression
+        if lag == 0:
+            print ("The regression is performed on instantaneous fields.")
+            if var_y.ndim == 1:
+                slope, _, r_value, p_value = stats.linregress(var_x, var_y)
+            elif var_y.ndim == 2:
+                t, y  = var_y.shape
+                slope = np.zeros(y, dtype=float)
+                r_value = np.zeros(y, dtype=float)
+                p_value = np.zeros(y, dtype=float)
+                for i in np.arange(y):
+                    slope[i], _, r_value[i], p_value[i] = stats.linregress(var_x[:,i], var_y[:,i])
+            elif var_y.ndim == 3:
+                t, y, x  = var_y.shape
+                slope = np.zeros((y, x), dtype=float)
+                r_value = np.zeros((y, x), dtype=float)
+                p_value = np.zeros((y, x), dtype=float)
+                for i in np.arange(y):
+                    for j in np.arange(x):
+                        slope[i,j], _, r_value[i,j], p_value[i,j] = stats.linregress(var_x[:,i,j], var_y[:,i,j])
+            else:
+                raise IOError("This module can not work with any array with a \
+                               dimension higher than 3!")              
+        elif lag > 0:
+            print ("This a regression with lead/lag analysis.")
+            
+        else:
+            IOError("The lead / lag coefficient should be positive integers.")
+            
+    
+    @staticmethod
+    def seasons(series, Dim_month=False):
+        """
+        Extract time series for summer / winter from given series.
+        The given time series should include the time series of all seasons.
+        Here summer is June, July and August, and winter is December, January and
+        February.
+        param series: input time series containing the data for all seasons.
+        param Dim_month: A check whether the time series include the dimension of month.
+        """
+        if Dim_month == True:
+            if series.ndim == 2:
+                t, m = series.shape
+                series = series.reshape(t*m)
+            elif series.ndim == 3:
+                t, m, y = series.shape
+                series = series.reshape(t*m, y)
+            elif series.ndim == 4:
+                t, m, y, x = series.shape
+                series = series.reshape(t*m, y, x)
+            else:
+                raise IOError("This module can not work with any array with a \
+                              dimension higher than 4!")
+        else:
+            pass
+        # seperate summer and winter from the rest of the months
+        if series.ndim == 1:
+            t = series.shape
+            series_summer = np.zeros(t/4,dtype=float)
+            series_winter = np.zeros(t/4,dtype=float)
+            # obtain summer time series
+            series_summer[0::3] = series[5::12] #June
+            series_summer[1::3] = series[6::12] #July
+            series_summer[2::3] = series[7::12] #August
+            # obtain winter time series
+            series_winter[2::3] = series[11::12]#December
+            series_winter[0::3] = series[0::12] #January
+            series_winter[1::3] = series[1::12] #February
+        elif series.ndim == 2:
+            t, y = series.shape
+            series_summer = np.zeros((t/4,y),dtype=float)
+            series_winter = np.zeros((t/4,y),dtype=float)
+              # obtain summer time series
+            series_summer[0::3,:] = series[5::12,:] #June
+            series_summer[1::3,:] = series[6::12,:] #July
+            series_summer[2::3,:] = series[7::12,:] #August
+            # obtain winter time series
+            series_winter[2::3,:] = series[11::12,:]#December
+            series_winter[0::3,:] = series[0::12,:] #January
+            series_winter[1::3,:] = series[1::12,:] #February              
+        elif series.ndim == 3:
+            t, y, x = series.shape
+            series_summer = np.zeros((t/4,y,x),dtype=float)
+            series_winter = np.zeros((t/4,y,x),dtype=float)
+              # obtain summer time series
+            series_summer[0::3,:,:] = series[5::12,:,:] #June
+            series_summer[1::3,:,:] = series[6::12,:,:] #July
+            series_summer[2::3,:,:] = series[7::12,:,:] #August
+            # obtain winter time series
+            series_winter[2::3,:,:] = series[11::12,:,:]#December
+            series_winter[0::3,:,:] = series[0::12,:,:] #January
+            series_winter[1::3,:,:] = series[1::12,:,:] #February
+        
+        return series_summer, series_winter
+        
+        
